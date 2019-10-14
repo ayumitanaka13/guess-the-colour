457025ff105592f10fb0006f6cf77d997168c7da
Ayumi Tanaka <tanakayumi13@gmail.com>
2019-10-14 16:54:08 +0900

    change script.js
    Add a function where if you give a correct answer, the background of the 
    body changes to the correct color. Also, implement a function to reset a 
    color when you push a reset button.

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ function makeColourValue(){
 }
 
 function startGame() {
+  let body = document.getElementsByTagName('body')[0];
   let answerMessage = document.getElementById('answer');
   let buttons = document.getElementsByClassName('colourButton');
   let answerButton = Math.round(Math.random() * (buttons.length-1));
   let heading = document.getElementById('colourValue');
   answerMessage.innerHTML = "";
+  setButtonColour(body, 255, 255, 255);
+
   for (let i = 0; i < buttons.length; i++) {
     let red = makeColourValue();
     let green = makeColourValue();
@@ -30,8 +33,8 @@ function startGame() {
     buttons[i].addEventListener('click', function(){
       if (this === buttons[answerButton]) {
         answerMessage.innerHTML = "Congrats!";
-        let body = document.getElementsByTagName('body')[0];
-        body.inner
+        setButtonColour(body, answerRed, answerGreen, answerBlue);
+
       } else {
         answerMessage.innerHTML = "You suck";
       }
