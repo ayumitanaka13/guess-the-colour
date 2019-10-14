9b8949a0abbaa4f5a7a6fb0ac39ea8b870f1ee95
gota_morishita <morishita_gota@cyberagent.co.jp>
2019-10-14 16:24:01 +0900

    add script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+function setButtonColour(button, red, green, blue) {
+  button.setAttribute('style', 'background-color: rgb(' + red +', ' + green +', ' + blue + ');');
+}
+
+function makeColourValue(){
+  return Math.round(Math.random()*255);
+}
+
+function startGame() {
+  let answerMessage = document.getElementById('answer');
+  let buttons = document.getElementsByClassName('colourButton');
+  let answerButton = Math.round(Math.random() * (buttons.length-1));
+  let heading = document.getElementById('colourValue');
+  answerMessage.innerHTML = "";
+  for (let i = 0; i < buttons.length; i++) {
+    let red = makeColourValue();
+    let green = makeColourValue();
+    let blue = makeColourValue();
+
+    setButtonColour(buttons[i], red, green, blue);
+
+    if (i === answerButton) {
+      heading.innerHTML = `(${red}, ${green}, ${blue})`;
+    }
+
+    let answerRed = red;
+    let answerGreen = green;
+    let answerBlue = blue;
+
+    buttons[i].addEventListener('click', function(){
+      if (this === buttons[answerButton]) {
+        answerMessage.innerHTML = "Congrats!";
+        let body = document.getElementsByTagName('body')[0];
+        body.inner
+      } else {
+        answerMessage.innerHTML = "You suck";
+      }
+    });
+  }
+}
+
+startGame();
+let resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', startGame);
